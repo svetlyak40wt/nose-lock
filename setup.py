3f97e552236319d0b57ac8358a51f8841ff6c4a7
Alexander Artemenko <svetlyak.40wt@gmail.com>
2015-03-06 11:56:34 +0300

    Add descriptions to setup.py.

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ setup(name='nose-lock',
       version='0.1.0',
       author='Alxander Artemenko',
       author_email='svetlyak.40wt@gmail.com',
+      description=('A plugin for python-nose which locks test execution '
+                   'and doesn\'t allow to run them in parallel '
+                   'from two terminals.'),
+      long_description=open('README.md').read(),
       packages=find_packages(),
       install_requires=['lockfile>=0.10,<0.11'],
       entry_points = {
